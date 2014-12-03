502c9a59349d90f6ea438caa998cec823a57d19e
NAYWAY <nathan95@gmail.com>
2014-12-03 02:41:45 -0500

    Update findaroom.js

--- findaroom/findaroom.js
+++ findaroom/findaroom.js
@@ -351,4 +351,27 @@ if (Meteor.isClient) {
 
   });
   
+  'doubletap .hammerDiv': function(event) {
+    alert("Double Tap!");
+  },
+  'hold .hammerDiv': function(event) {
+    alert("Hold!");
+  },
+  'pinchin .hammerDiv': function(event) {
+    alert("Pinch In!");
+  },
+  'pinchout .hammerDiv': function(event) {
+    alert("Pinch Out!");
+  },
+  'swiperight .hammerDiv': function(event) {
+    alert("Swipe Right!");
+  }
+    
+
+  });
+  
+  UI.body.rendered = function(){
+  $('body').hammer();
+  }
+  
 }
