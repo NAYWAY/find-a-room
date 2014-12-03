7bb0668b7c2cb2fe9a95853cd82d24ba564159f1
NAYWAY <nathan95@gmail.com>
2014-12-03 02:54:30 -0500

    Update findaroom.js

--- findaroom/findaroom.js
+++ findaroom/findaroom.js
@@ -2,18 +2,24 @@ var IMAGE_KEY = "qrcode-img";
 
 var gCtx = null;
 var gCanvas = null;
+var dCanvas = null;
 
 var Rooms = new Meteor.Collection("rooms");
 var Facilities = new Meteor.Collection("facilities");
 var Buildings = new Meteor.Collection("buildings");
-
+var Lines = new Meteor.Collection("lines"); // for navigation.
 var current_bldg; // this varibale will be initailed with the GPS
 var current_bldg_img;
+var Sugg = [];
 
-//auto fill
-//   let's say the string is called "result"
-//   so it's
-
+/*Meteor.defer(function(){
+  var Ham = ("testPinch");
+  Ham.on('hold tap swipe doubletap transformstart transform transformend dragstart drag dragend swipe release', function (event) {
+      event.preventDefault();
+      console.log("Type: " + event.type + ", Fingers: " + event.touches.length + ", Direction: " + event.direction + "<br/>");
+      });
+}
+)*/
 
 var mapcanvas = null;
     mapcontext = null;
@@ -47,7 +53,7 @@ if (Meteor.isServer) {
       Rooms.insert({ bldg: "LWSN", floor: "B", room: "130", xpix: 397, ypix: 1130, popular: false } );
       Rooms.insert({ bldg: "LWSN", floor: "B", room: "128", xpix: 397, ypix: 1275, popular: false } );
       Rooms.insert({ bldg: "LWSN", floor: "B", room: "129", xpix: 303, ypix: 1235, popular: false } );
-      Rooms.insert({ bldg: "LWSN", floor: "B", room: "116", xpix: 395, ypix: 1520, popular: true } );
+      Rooms.insert({ bldg: "LWSN", floor: "B", room: "116", xpix: 395, ypix: 1420, popular: true } );
       Rooms.insert({ bldg: "LWSN", floor: "B", room: "105", xpix: 219, ypix: 1457, popular: false } );
       Rooms.insert({ bldg: "LWSN", floor: "B", room: "107", xpix: 250, ypix: 1458, popular: false } );
 
@@ -61,10 +67,42 @@ if (Meteor.isServer) {
          Facilities.insert({ bldg: "LWSN", floor: "B",type:"Elevator",room:"1", xpix: 317, ypix: 800 });    //elevator
 
     }
+    if(Lines.find().count() == 0) {
+        Lines.insert( { bldg: "LWSN", floor: "B", xpix: 88, ypix: 1486, description: "You should see the exit" });
+        Lines.insert( { bldg: "LWSN", floor: "B", xpix: 354, ypix: 1486, description: "You should see Room 116" });
+        Lines.insert( { bldg: "LWSN", floor: "B", xpix: 354, ypix: 37, description: "You should see a vending machine" });
+        Lines.insert( { bldg: "LWSN", floor: "B", xpix: 487, ypix: 37, description: "You should see the exit" });
+    }
   })
 }
 
-
+// function finding a point () say given a point p {int xpix, int ypix}
+function closestNode(p)
+{
+    var l;
+    var distance = new Array(4);
+    for(i = 0; i < 4; i++) {
+      l = Lines.findOne( { bldg: "LWSN", floor: "B"}, {skip:i});
+      if (l == null)  break;
+      distance[i] = Math.sqrt((l.xpix-p.xpix)*(l.xpix-p.xpix)+(l.ypix - p.ypix)*(l.ypix - p.ypix));
+    }
+    var closest = 0;
+    for(i = 1; i < 4; i++) {
+      if(distance[i] < distance[closest])
+          closest = i;
+    }
+    l = Lines.findOne( { bldg: "LWSN", floor: "B"},{skip:closest});
+    var node = {xpix:l.xpix, ypix:l.ypix};
+    var pclose = {xpix:p.xpix, ypix:p.ypix};
+    if(Math.abs(pclose.xpix-node.xpix) < Math.abs(pclose.ypix-node.ypix))
+      pclose.xpix = node.xpix;
+    else if(Math.abs(pclose.xpix-node.xpix) == Math.abs(pclose.ypix-node.ypix))
+      pclose.xpix = -1;
+    else
+      pclose.ypix = node.ypix;
+    var closePoints= [node,pclose];
+    return closePoints; // closest node is [0], the corresponding room pixels is [1];
+}
 
 function restroom()
 {
@@ -88,12 +126,162 @@ function restroom()
     return locations_coordinate;
 }
 
+function make_point(x,y)
+{
+
+    var coordinate;
+    coordinate=(
+        {"xpix":x,
+         "ypix":y
+        });
+    return coordinate;
+}
+
+function on_the_line(x1,y1,x2,y2,x3,y3) //check if (x3,y3) is on segment (x1,y1), (x2,y2) now only for straight line
+{
+    if ((x3==x1) && (x3==x2))
+    {
+        yhat1=y3-y1;
+        yhat2=y3-y2;
+        
+        alert(yhat1*yhat2);
+        if (yhat1*yhat2<0)
+            return true;
+    }
+    
+    if ((y3==y1) && (y3==y2))
+    {
+        xhat1=x3-x1;
+        xhat2=x3-x2;
+        alert(xhat1*xhat2);
+        if (xhat1*xhat2<0)
+            return true;
+    }
+    if (x1==x2 && y1==y2)
+        return true;
+    return false;
+}
+
+function check_the_turn(x0,y0,x1,y1,x2,y2) //1 left -1  right
+{
+    ans=(x1-x0)*(y2-y0)-(x2-x0)*(y1-y0);   
+    if (ans>0) return 1;
+    if (ans==0) return 0;
+    if (ans<0) return -1;
+}
+function find_destination(startx,starty,endx,endy)
+{
+    start_point=make_point(startx,starty);
+    end_point=make_point(endx,endy);
+    
+    console.log(start_point.xpix);
+    
+    startx=closestNode(start_point)[0].xpix;
+    starty=closestNode(start_point)[0].ypix;
+    
+    endx=closestNode(end_point)[0].xpix;
+    endy=closestNode(end_point)[0].ypix;
+    
+    
+    
+    var queue=[];
+    queue.push({coordinate:make_point(startx,starty),prev:0,distance:0});
+    
+    nowx=startx;
+    nowy=starty;
+    head=0;
+    tail=0;
+    while (nowx!=endx && nowy!=endy)
+    {
+        for (counter=0;;counter++)
+        {
+            var current=Lines.findOne({'$or': [ {'xpix':nowx ,'ypix' : {$ne : nowy} }, { 'ypix': nowy, 'xpix' : {$ne : nowx}}]} ,{skip:counter});  // nowx==xpix xor nowy==ypix
+                                               
+                                    //make sure the x and y is the same in the future
+            if (current==null) break;
+            tail+=1;
+            queue.push({xpix:current.xpix,ypix:current.ypix,prev:head,distance:queue[head].distance+1});
+        }
+        head+=1;
+        nowx=queue[head].xpix;
+        nowy=queue[tail].ypix;
+    }
+    
+    now=tail;
+    point_list=[];
+    point_list.push({xpix:endx,ypix:endy});
+    for (counter=0;;counter++)
+    {
+  //      alert("now "+now+" "+"xpix "+queue[now].xpix+"ypix "+queue[now].ypix);
+     
+        if (now==0)
+            break;
+        point_list.unshift({xpix:queue[now].xpix,ypix:queue[now].ypix});
+        now=queue[now].prev;
+        
+        //alert(now);
+    }
+    point_list.unshift({xpix:startx,ypix:starty});
+    
+
+    
+    length=point_list.length;
+    flag=0;
+    
+    if ((length==2) && (point_list[0].xpix==point_list[1].xpix) && (point_list[0].ypix==point_list[1].ypix))
+        flag=1;
+                                                                   
+    if (on_the_line(point_list[0].xpix,point_list[0].ypix,point_list[1].xpix,point_list[1].ypix,closestNode(start_point)[1].xpix,closestNode(start_point)[1].ypix)==true || flag==1)
+    {
+        point_list[0].xpix=closestNode(start_point)[1].xpix;
+        point_list[0].ypix=closestNode(start_point)[1].ypix;
+        
+    }
+    else
+    {
+        point_list.unshift({xpix:closestNode(start_point)[1].xpix,ypix:closestNode(start_point)[1].ypix});
+    }
+    
+    if (on_the_line(point_list[length-1].xpix,point_list[length-1].ypix,point_list[length-2].xpix,point_list[length-2].ypix,closestNode(end_point)[1].xpix,closestNode(end_point)[1].ypix)==true || flag==1)
+    {
+        point_list[length-1].xpix=closestNode(end_point)[1].xpix;
+        point_list[length-1].ypix=closestNode(end_point)[1].ypix;
+    }
+    else
+    {
+        point_list.push({xpix:closestNode(end_point)[1].xpix,ypix:closestNode(end_point)[1].ypix});
+    }
+    
+    //alert("nearest node"+closestNode(start_point)[1].xpix+"y: "+closestNode(start_point)[1].ypix);
+    
+    point_list.unshift({xpix:start_point.xpix,ypix:start_point.ypix});
+    point_list.push({xpix:end_point.xpix,ypix:end_point.ypix});
+    
+    for (i=1; i<point_list.length-1; i++)
+    {
+       alert(point_list[i].xpix+"  "+point_list[i].ypix); //alert(check_the_turn(point_list[0].xpix,point_list[0].ypix,point_list[1].xpix,point_list[1].ypix,point_list[2].xpix,point_list[2].ypix));
+      if (check_the_turn(point_list[i-1].xpix,point_list[i-1].ypix,point_list[i].xpix,point_list[i].ypix,point_list[i+1].xpix,point_list[i+1].ypix)==-1)
+          string=" TURN FUCKING LEFT ";
+        else
+            string=" TURN FUCKING RIGHT ";
+       if (Lines.findOne({xpix:point_list[i].xpix,ypix:point_list[i].ypix})!=null)
+         alert(Lines.findOne({xpix:point_list[i].xpix,ypix:point_list[i].ypix}).description+string);
+        else
+            if (i==1)
+            alert("get the fuck out of here and"+string);
+        else alert("go fucking inside!!! You ARE DONE" +string);
+    }
+    
+    
+}
+
 function autofill_room(result)
 {
+  Sugg = [];
   var rs;
   var auto = [];
     current_bldg=Session.get("bldg");
- for (counter=0;;counter++)
+ for (counter=0;counter < 5;counter++)
     {
   if(result.length == 1)
    rs = Rooms.findOne( { bldg: current_bldg, floor: result, popular: true }, {skip:counter, _id: 0, floor: 1, room: 1});
@@ -102,7 +290,13 @@ function autofill_room(result)
         if (rs==null) break;
          var string=rs.floor+rs.room;
         auto.push(string);
+        Sugg.push(string);
     }
+    if(Sugg.length == 0)
+    {
+        Sugg[0] = "No Match Found";
+    }
+    Session.set("sugg", Sugg);
     console.log(auto);
     return auto;
 }
@@ -125,20 +319,46 @@ function load()
   Session.set("navReady",0);
 }
 
+function drawLine(x1, y1, x2, y2)
+{
+  gCanvas = document.getElementById("draw-line");
+  console.log("draw a line");
+  var ctx = gCanvas.getContext("2d");
+  ctx.beginPath();
+  ctx.moveTo(x1,y1);
+  ctx.lineTo(x2,y2);
+  ctx.lineWidth = 5;
+  ctx.strokeStyle = '#4780A6';
+  ctx.stroke();
+}
+
 // simple-todos.js
 if (Meteor.isClient) {
   // This code only runs on the client
-
+  //find_destination(308,361,396,349);
   Template.home.created = function(){
     if(Session.get("scan")==1)
       drawStuff();
-
+    $(function() {
+    $(window).on('resize', function resize()  {
+        $(window).off('resize', resize);
+        setTimeout(function () {
+            var content = $('#mid-window');
+            console.log("center");
+            var top = (window.innerHeight - content.height()) / 2;
+            content.css('top', Math.max(0, top) + 'px');
+            $(window).on('resize', resize);
+        }, 50);
+    }).resize();
+   });
   };
 
   Meteor.startup(function () {
     Session.set("posX", 160);
     Session.set("posY", -100);
+    Session.set("navTop",-200+"px");
     load();
+    
   });
   Meteor.setInterval(function() {
     navigator.geolocation.getCurrentPosition(function(position) {
@@ -188,7 +408,14 @@ if (Meteor.isClient) {
       return Session.get("destination");
     },
     navReady: function(){
-      return Session.get("navReady"); 
+      return Session.get("navReady");
+    },
+    navTop: function(){
+      return Session.get("navTop"); 
+    },
+    getSugg: function(){
+      if(Session.get("navReady") !== 1)
+      return Session.get("sugg");
     }
   });
 
@@ -239,18 +466,18 @@ if (Meteor.isClient) {
       current_bldg = Buildings.findOne( { highLatitude: { $gte: lat}, lowLatitude: { $lte: lat}, highLongitude: { $gte: log}, lowLongitude: { $lte: log} }, { _id: 0, bldg: 1} );
     },
     'submit .new-task': function(event) {
-        
+
         result=event.target.text.value.replace(/\s+/g, '');
         var f = result.charAt(0);
         var r = result.substring(1);
 
         var response = Rooms.findOne( { room: r, floor: f },{_id:0,xpix:1});
-      
+
         if(response==undefined){
           alert("Room not found");
           return false;
         }
-      
+
         Session.set("scan",1);
 
         posx= response.xpix;
@@ -261,7 +488,12 @@ if (Meteor.isClient) {
         Session.set("bldg", response.bldg);
         Session.set("mapimg", response.bldg+"_"+response.floor+".jpg");
         Session.set("location", result );
-
+        $( document ).ready(function() {
+          console.log( "ready!" );
+          $('html, body').animate({
+            scrollTop: (posy*320/800-50)+"px"
+          }, 800);
+        });
         return false;
     },
 
@@ -289,27 +521,27 @@ if (Meteor.isClient) {
         Session.set("navReady",0);
     },
     'click .fa-times': function(){
-        $("#search-main").val("").focus(); 
+        $("#search-main").val("").focus();
     },
     'click .dest_desc': function(){
         if( Session.get("navReady")!=0)
           Session.set("navReady",0);
-        else  
+        else
           Session.set("navReady",1);
     },
     'submit .search-dest': function(event, template) {
         Session.set("scan",1);
         var re = event.target.text.value.replace(/\s+/g, '');
-      
+
         var f = re.charAt(0);
         var r = re.substring(1);
 
         var response = Rooms.findOne( { room: r, floor: f },{_id:0,xpix:1});
 
          if(response==undefined){
-        
+
             template.find(".search-main").blur();
-           
+
             $("#search-main")
               .css("background-color","rgb(232, 149, 149)")
               .css("color","rgb(136, 15, 15)")
@@ -317,37 +549,100 @@ if (Meteor.isClient) {
               .css("font-size","14px");
             $(".fa-search").css("color","rgb(174, 40, 40)")
               .addClass("fa-times");
-            Session.set("navReady",0); 
+            Session.set("navReady",0);
             Session.set("posX", 160);
             Session.set("posY", -100);
-           
+
             return false;
          }
-      
-        posx= response.xpix;
-        posy= response.ypix;
 
-        Session.set("posX", posx);
-        Session.set("posY", posy);
+        psx= response.xpix;
+        psy= response.ypix;
+
+        Session.set("posX", psx);
+        Session.set("posY", psy);
         Session.set("destination", re );
         template.find(".search-main").blur();
         $("#search-main")
-          .css("background-color","rgb(208, 232, 149)")
-          .css("color","rgb(100, 136, 15)")
           .css("font-weight","bold")
           .css("font-size","14px");
-        $(".fa-search").css("color","rgb(134, 174, 40)").addClass("fa-check");;
+        $(".fa-search").css("color","rgb(195, 219, 137)").addClass("fa-check");;
         Session.set("navReady",1);
-        
+        $( document ).ready(function() {
+          console.log( "ready!" );
+          $('html, body').animate({
+            scrollTop: (psy*320/800-50)+"px"
+          }, 800);
+        });
+        //drawLine(posx*320/800+'px', posy*320/800+'px', Session.get("posX")*320/800+'px', Session.get("posY")*320/800+'px')
         return false;
     },
 
-   'keyup .search-dest': function(event) {
+    'keyup .search-dest': function(event) {
 
        // console.log( document.getElementById('search-main').value );
 
         autofill_room(document.getElementById('search-main').value);
     },
+    
+    'click .startnav': function(event){
+        
+        
+        start=Session.get("location");
+        dest=Session.get("destination");
+        var f = start .charAt(0);
+        var r = start.substring(1);
+
+        var start_document = Rooms.findOne( { room: r, floor: f });
+        var f = dest .charAt(0);
+        var r = dest.substring(1);
+
+        var dest_document = Rooms.findOne( { room: r, floor: f });
+        find_destination(start_document.xpix,start_document.ypix,dest_document.xpix,dest_document.ypix);
+        
+        Session.set("navTop",0); 
+        Session.set("navReady",0);
+        
+    },
+    
+    'click .closebtn': function(event){
+        Session.set("navTop",-200+"px"); 
+    },
+    
+    'click .setDestination': function(event){
+      
+      
+        var re = event.target.textContent;
+        
+        var f = re.charAt(0);
+        var r = re.substring(1);
+
+        var response = Rooms.findOne( { room: r, floor: f },{_id:0,xpix:1});
+
+        psx= response.xpix;
+        psy= response.ypix;
+
+        Session.set("posX", psx);
+        Session.set("posY", psy);
+        Session.set("destination", re );
+        Sugg = [];
+        Session.set("sugg",Sugg);
+        $("#search-main").blur();
+        $("#search-main")
+          .css("font-weight","bold")
+          .css("font-size","14px");
+        $(".fa-search").css("color","rgb(195, 219, 137)").addClass("fa-check");;
+        $("#search-main").val(re);
+        Session.set("navReady",1);
+        
+         $( document ).ready(function() {
+          console.log( "ready!" );
+          $('html, body').animate({
+            scrollTop: (psy*320/800-50)+"px"
+          }, 800);
+        });
+      
+    }, 
     
   'doubletap .hammerDiv': function(event) {
     alert("Double Tap!");
@@ -371,5 +666,6 @@ if (Meteor.isClient) {
   UI.body.rendered = function(){
   $('body').hammer();
   }
+
   
 }
